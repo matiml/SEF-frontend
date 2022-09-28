b928ab71ed4ec570304e2eff5fa7af1c2e9636ca
Leandro <lean.arbelo.dev@gmail.com>
2022-09-28 13:38:08 -0300

    cambie rutas del fetch en todos los componentes

--- src/components/MessagesColumn/MessagesColumn.jsx
+++ src/components/MessagesColumn/MessagesColumn.jsx
@@ -11,7 +11,7 @@ function MessagesColumn({ selectedClient = {}, selectedSeller = {} }) {
 
     const mensajesFetch = async() => {
         // '/vendedores/${selectedClient.id}/mensajes'
-        const response = await fetch(`https://sef-production.up.railway.app/vendedores/${selectedClient.id}/mensajes`);
+        const response = await fetch(`https://sef-production-a2d4.up.railway.app/vendedores/${selectedClient.id}/mensajes`);
         const json = await response.json();
         setMessages(json);
     }
